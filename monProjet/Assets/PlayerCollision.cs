a7b18debbbfe02975f20f7ddca53f8a4ee7cc043
HappyVince <59864690+HappyVince@users.noreply.github.com>
2020-03-16 10:14:22 +0100

    da
    da

---
+++ monProjet/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMouvement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
